49f99b312af8fbe635e39a8eaee3ad426e994cc9
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-04-14 11:35:22 +0300

    new version

---
+++ myBank/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myBank
+//
+//  Created by Админ on 14.04.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
